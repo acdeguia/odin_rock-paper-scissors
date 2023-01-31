aeceae2039d6ecc2074481cad1b7b5d27750cd5e
Cate <anncatherinedeguia@gmail.com>
2023-01-31 16:26:53 +0800

    Created three buttons one for each selection

--- javascript.js
+++ javascript.js
@@ -3,6 +3,10 @@ let computerSelection;
 let userScore = 0;
 let computerScore = 0;
 let draw = 0;
+let getPlayerChoice = ''
+
+const result = document.querySelector('result')
+
 
 function getComputerChoice() {
     let array = ['rock', 'paper', 'scissors']
@@ -10,15 +14,19 @@ function getComputerChoice() {
     return randomElement;
 }
 
-function getPlayerChoice(){
-    const playerChoice = prompt('Choose between rock paper scissors');
-    return playerChoice;
-}
+//this function prompts the user to choose between rock, paper or scissors.
+
+// function getPlayerChoice(){
+//     const playerChoice = prompt('Choose between rock paper scissors');
+//     return playerChoice;
+// }
+
+//this function plays a single round of rock, paper, scissors using if else loop. And it also declares the winner at the end.
 
 function playRound(playerSelection, computerSelection) {
 
-    playerSelection = getPlayerChoice().toLowerCase();
-    console.log(playerSelection);
+    playerSelection = getPlayerChoice.toLowerCase();
+    console.log(playerSelection); 
 
     computerSelection = getComputerChoice().toLowerCase();
     console.log(computerSelection);
@@ -47,19 +55,46 @@ function playRound(playerSelection, computerSelection) {
     }
  }
  
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    console.log('Player Score: ' + userScore);
-    console.log('Computer Score: ' + computerScore);
-    console.log('Draw:' + draw);
+//  This function calls the playRound function inside of this to play 5 rounds and reports the winner at the end.
 
-    if(userScore > computerScore){
-        return('congratulations!').toUpperCase()
-    }else{
-        return 'better luck next time'.toUpperCase();
-    }
-}
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+//     console.log('Player Score: ' + userScore);
+//     console.log('Computer Score: ' + computerScore);
+//     console.log('Draw:' + draw);
 
-console.log(game());
+//     if(userScore > computerScore){
+//         return('congratulations! You won').toUpperCase()
+//     }else{
+//         return 'better luck next time'.toUpperCase();
+//     }
+// }
+
+// console.log(game());
+
+
+// console.log(playRound(playerSelection, computerSelection));
+
+
+let rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+    getPlayerChoice = 'rock'
+    console.log(playRound())
+    
+});
+
+let paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+    getPlayerChoice = 'paper'
+    console.log(playRound())
+});
+
+let scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+    getPlayerChoice = 'scissors'
+    console.log(playRound())
+});
+
+
